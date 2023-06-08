ec243dacf3ce564f37b4e083548e4468c27fe66b
Flynn <wphestiraid@gmail.com>
2023-06-09 08:44:27 +0900

    :necktie: 새로운 할인 정책 개발

---
+++ src/main/java/dev/flynnpark/springcorebasics/discount/RateDiscountPolicy.java
@@ -0,0 +1,16 @@
+package dev.flynnpark.springcorebasics.discount;
+
+import dev.flynnpark.springcorebasics.member.Grade;
+import dev.flynnpark.springcorebasics.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPrice = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPrice / 100;
+        }
+        return 0;
+    }
+}
