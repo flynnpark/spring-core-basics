5435c2e628988ec072a7ada5e6a9dd75709fd100
Flynn <wphestiraid@gmail.com>
2023-06-13 09:08:17 +0900

    :recycle: AppConfig 리팩터링

--- src/main/java/dev/flynnpark/springcorebasics/AppConfig.java
+++ src/main/java/dev/flynnpark/springcorebasics/AppConfig.java
@@ -1,6 +1,8 @@
 package dev.flynnpark.springcorebasics;
 
+import dev.flynnpark.springcorebasics.discount.DiscountPolicy;
 import dev.flynnpark.springcorebasics.discount.FixDiscountPolicy;
+import dev.flynnpark.springcorebasics.member.MemberRepository;
 import dev.flynnpark.springcorebasics.member.MemberService;
 import dev.flynnpark.springcorebasics.member.MemberServiceImpl;
 import dev.flynnpark.springcorebasics.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import dev.flynnpark.springcorebasics.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
