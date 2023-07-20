b7c430be913c33fa6c03b76ecefb0ab0b309d75c
Flynn <wphestiraid@gmail.com>
2023-07-20 09:21:08 +0900

    :sparkles: 의존관계 자동 주입 - 롬복과 최신 트렌드

--- src/main/java/dev/flynnpark/springcorebasics/order/OrderServiceImpl.java
+++ src/main/java/dev/flynnpark/springcorebasics/order/OrderServiceImpl.java
@@ -3,19 +3,15 @@ package dev.flynnpark.springcorebasics.order;
 import dev.flynnpark.springcorebasics.discount.DiscountPolicy;
 import dev.flynnpark.springcorebasics.member.Member;
 import dev.flynnpark.springcorebasics.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
