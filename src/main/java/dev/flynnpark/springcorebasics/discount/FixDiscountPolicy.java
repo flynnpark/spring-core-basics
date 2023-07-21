2d8d0eabee5e7cbb1b0740b7b65728ea3aeb29be
Flynn <wphestiraid@gmail.com>
2023-07-21 09:04:01 +0900

    :goal_net: 조회 빈이 2개 이상일 때의 문제

--- src/main/java/dev/flynnpark/springcorebasics/discount/FixDiscountPolicy.java
+++ src/main/java/dev/flynnpark/springcorebasics/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package dev.flynnpark.springcorebasics.discount;
 
 import dev.flynnpark.springcorebasics.member.Grade;
 import dev.flynnpark.springcorebasics.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000;
 
